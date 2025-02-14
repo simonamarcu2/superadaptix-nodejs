81accc410c3edc71b8fba80895e66e4dcc2cf807
Simona Marcu <supersimy16@gmail.com>
2025-02-14 13:42:22 +0000

    ~adding data to gantt
    Refactor Gantt chart integration and update project metadata

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 1337;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.all(/api/, function(req, res, next) {
+  console.log(`\n${req.method} ${req.url} -->${JSON.stringify(req.body,'\t',2)}`);
+  res.status(200).end;
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
